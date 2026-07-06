72904a90fd49a3956db9dfb1ac6d319f3bab41ee
MenkeTechnologies <linux.dev25@gmail.com>
2026-07-05 22:13:05 -0400

    zemacs: bump submodule to v0.3.1 docs+man pages; enforce zemacs in tier1 man-page gate

--- tests/tier1-man-pages.sh
+++ tests/tier1-man-pages.sh
@@ -51,6 +51,7 @@ primary_bin[powerliners]="powerline"
 primary_bin[storageshower]="storageshower"
 primary_bin[ztmux]="ztmux"
 primary_bin[htoprs]="htoprs"
+primary_bin[zemacs]="zemacs"
 
 checked=0
 missing=0
